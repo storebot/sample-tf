1b9db7807ca7252529d7f636d95f3d0b3042a4a6
Iceal <storebot@androidph.com>
2020-09-20 15:23:27 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,15 +6,8 @@ resource "aws_s3_bucket" "demo-s3" {
   bucket = "${var.s3_bucket_prefix}-terraformcloud"
   versioning {
     enabled = false
-    mfa_delete = true
+    mfa_delete = false
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }  
 }
 
 variable "s3_bucket_prefix" {
