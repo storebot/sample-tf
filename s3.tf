30557054a1490b1ac12281c5adac57f179d48d5f
Iceal <storebot@androidph.com>
2020-10-03 17:24:06 -0700

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@
 # used for prod
 # region us-east-1
 resource "aws_s3_bucket" "backup_s3" {
-  bucket = "prod_backups10"
+  bucket = "prod_backups11"
   versioning {
     enabled    = false
     mfa_delete = false
